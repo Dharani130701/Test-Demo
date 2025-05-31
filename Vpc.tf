bfbf8488622d4e50f37263697bc54113d1a541d8
Dharani-p130701 <pdharani142020@gmail.com>
2025-05-31 14:11:15 +0530

    commit

---
+++ Vpc.tf
@@ -0,0 +1,36 @@
+    resource "aws_vpc" "Test_vpc" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "TestVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.Test_vpc.id
+  cidr_block = "10.1.0.0/18"   # 10.1.0.0 - 10.1.63.255
+
+  tags = {
+    Name = "TestPS"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.Test_vpc.id
+  cidr_block = "10.1.64.0/18"  # 10.1.64.0 - 10.1.127.255 (next /18 block)
+
+  tags = {
+    Name = "TestPVT"
+  }
+}
+resource "aws_internet_gateway" "test_igw"{
+    vpc_id = aws_vpc.Test_vpc.id
+}
+resource "aws_route_table" "test_route" {
+  vpc_id = aws_vpc.Test_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test_igw.id
+  }
+}
--- ec.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "this" {
-  ami                     = "ami-0c1ac8a41498c1a9c"
-  instance_type           = "t3.micro"
-}
- # code for ec2 creation in vs using tf#
